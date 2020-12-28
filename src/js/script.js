5c5ba86701c52949333c799c718dc95e3e2b8a51
Pavel <p.gamov2000@yandex.ru>
2020-12-29 00:13:48 +0300

    Add all blocks + modal

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    
+
+    // Modal 
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks').fadeOut('slow');
+    })
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.button_mini').on('click', function() {
+        $('.overlay, #order').fadeIn('slow');
+    });
+
+    function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: "required",
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: "Пожалуйста ввеедите свое имя",
+            phone: "Пожалуйста введите совй номер",
+            email: {
+              required: "Пожалуйста ввеедите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
+          }
+    }); 
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+
+
+    $('input[name=phone]').mask("+7(999) 999-9999");
+    
+
+  });
